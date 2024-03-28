f956e95189f19e8fa4983f54033daa8e0f29fffa
sarahashe <sashe@umd.edu>
2024-03-27 20:39:57 -0400

    Wrote the favorite-course.html template to accept two query parameters of your favorite course, passed via URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,12 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('You entered your favorite course as: ' + request.args.get('subject') + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
 
 if __name__ == '__main__':
     app.run()
